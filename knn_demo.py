06466fcc6e21f677f1eaac0baa923907501f32b1
steve shen <349351805@qq.com>
2017-11-20 21:53:05 +0800

    knn for num recgonition

--- knn_demo.py
+++ knn_demo.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import sys,os
 from numpy import *
 import matplotlib
@@ -34,14 +35,14 @@ def file2matrix(filename):   #文件读入
     index = 0
     for line in arrayOLines:
         line = line.strip()
-        listFormLine = line.split('\t')
-        returnMat[index,:] = listFormLine[0:3]
-        classLabelVector.append(int(listFormLine[-1]))
+        listFromLine = line.split('\t')
+        returnMat[index,:] = listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
         index += 1
     return returnMat,classLabelVector
 
 def autoNorm(dataSet):
-    #归一化特征值，用于加权平衡变量
+    #归一 化特征值，用于加权平衡变量
     minVals = dataSet.min(0)
     maxVals = dataSet.max(0)
     ranges = maxVals - minVals
@@ -51,6 +52,31 @@ def autoNorm(dataSet):
     normDataSet = normDataSet/tile(ranges,(m,1))
     return normDataSet,ranges,minVals
 
+def datingClassTest():
+    #分类器针对约会网站的测试代码
+    hoRadio = 0.10
+    datingDataMat,datingLabels = file2matrix('datingTestSet2.txt')
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m*hoRadio)
+    errorCount = 0.0
+    for i in range(numTestVecs):
+        classifierResult = classify0(normMat[i,:],normMat[numTestVecs:m,:],datingLabels[numTestVecs:m],3)
+        print "the classifier came back with: %d,the real answer is: %d"%(classifierResult,datingLabels[i])
+        if (classifierResult != datingLabels[i]):errorCount += 1.0
+    print "the total error rate is: %f"%(errorCount/float(numTestVecs))
+
+def classifyPerson():
+    resultList = ['not at all','in small doses','in large doses']
+    percentTats = float(raw_input("percentage of time spent playing video games?"))
+    ffMiles = float(raw_input("frequent filter miles earned per year?"))
+    iceCream = float(raw_input("liters of ice cream consumed per year?"))
+    datingDataMat,datingLabels = file2matrix('datingTestSet2.txt')
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    inArr = array([ffMiles,percentTats,iceCream])
+    classifierResult = classify0((inArr-minVals)/ranges,normMat,datingLabels,3)
+    print "You will probably like this person: ", resultList[classifierResult-1]
+
 
 def main():   #主程序清单
     datingDataMat,datingLabels = file2matrix('datingTestSet2.txt')
@@ -58,7 +84,7 @@ def main():   #主程序清单
     ax = fig.add_subplot(111)
     ax.scatter(datingDataMat[:,1],datingDataMat[:,2])
     plt.show()
-
+    datingClassTest()
 
 if __name__=="__main__":
      main()

88c4f5dcd7062174f77752d0ed9f4facc9c28fe4
Steve Shen <349351805@qq.comgit config --global user.email 349351805@qq.com>
2018-01-11 16:49:24 +0800

    1

--- bayes_demo.py
+++ bayes_demo.py
@@ -93,22 +93,67 @@ def bagOfWord2VecMN(vocabList,inputSet):
             returnVec[vocabList.index(word)] += 1
     return returnVec
 
-def main():   #主程序清单
-    listOPosts,listClasses = loadDataSet()
-    myvocabList = createVocabList(listOPosts)
+def textParse(bigString):
+    import re
+    listOfTokens = re.split(r'W*',bigString)
+    return [tok.lower()for tok in listOfTokens if len(tok)>2]           #剔除分词结果中长度小于2的单词
 
-    # print myvocabList
+def spamTest():
+    docList = []
+    classList = []
+    fullText = []
+    for i in range(1,26):
+        wordList = textParse(open('email/spam/%d.txt'%i).read())
+        docList.append(wordList)
+        fullText.append(wordList)
+        classList.append(1)
+        wordList = textParse(open('email/ham/%d.txt'%i).read())
+        docList.append(wordList)
+        fullText.append(wordList)
+        classList.append(0)
 
-    # print setOfWords2Vec(myvocabList,listOPosts[0])
-    # print setOfWords2Vec(myvocabList,listOPosts[3])
+    vocabList = createVocabList(docList)
+    trainingSet = range(50)
+    testSet = []
+
+    for i in range(10):
+        randIndex = int(random.uniform(0,len(trainingSet)))
+        testSet.append(trainingSet[randIndex])
+        del(trainingSet[randIndex])
 
     trainMat = []
-    for postinDoc in listOPosts:
-        trainMat.append(setOfWords2Vec(myvocabList,postinDoc))
+    trainClasses = []
+    for docIndex in trainingSet:
+        trainMat.append(setOfWords2Vec(vocabList,docList[docIndex]))
+        trainClasses.append(classList[docIndex])
 
-    p0V,p1V,pab = trainNB0(trainMat,listClasses)
+    p0V,p1V,pSpam = trainNB0(array(trainMat),array(trainClasses))
+    errorCount = 0
+    for docIndex in testSet:
+        wordVector = setOfWords2Vec(vocabList,docList[docIndex])
+        if classifyNB(array(wordVector),p0V,p1V,pSpam)!=classList[docIndex]:
+            errorCount += 1
 
-    print p0V,p1V,pab
+    print 'the error rate is',float(errorCount/len(testSet))
+
+def main():   #主程序清单
+    # listOPosts,listClasses = loadDataSet()
+    # myvocabList = createVocabList(listOPosts)
+    #
+    # # print myvocabList
+    #
+    # # print setOfWords2Vec(myvocabList,listOPosts[0])
+    # # print setOfWords2Vec(myvocabList,listOPosts[3])
+    #
+    # trainMat = []
+    # for postinDoc in listOPosts:
+    #     trainMat.append(setOfWords2Vec(myvocabList,postinDoc))
+    #
+    # p0V,p1V,pab = trainNB0(trainMat,listClasses)
+    #
+    # print p0V,p1V,pab
+
+    spamTest()
 
 
 if __name__=="__main__":
